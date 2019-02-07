4d4b8d7d0a2cae3a7427fc930cfb48e88b90086e
emily <emily.b566@stu.nebo.edu>
2019-02-07 16:03:32 -0700

    working on it

--- game.js
+++ game.js
@@ -35,11 +35,13 @@ function Game(){
 	function Prison(){
 		var prison = prompt("There has been a pandemic they had kept the disease in a town that was fenced, you were a guard of the diseased you didn't know how they had plotted a plan to escape. One day when you were giving the sick food and then they grabbed you and drug you somewhere. You just barley woke up. You have no clue where everyone is. You no longer have your safteysuit from the disease on. You look around adn see a purple,green,balck,mark on your arm, it is the rash of the disease. /n-look around /n-go back to sleep /n-freak out /n-go to the grass").toLowerCase();
     
-        
+    
 		
 		if(prison == "look around" || prison == "look"){
 			 var exitprison = prompt(" You sit up from you cot. The door to the room is open. You see vibrant green grass with a lake in the distance.  /n -go to the lake /n -stay in the room a look around the room more").toLowerCase;
-		}
+            Lake();
+        }
+		
 			
 		else if(prison == "go to the grass" || prison == "grass"){
 			alert("You are now outside. You see flowers decerating the edge of the house. To your left you see a giant garden");
@@ -83,6 +85,7 @@ function Game(){
             Prison();
         }
 	}
+}
     Lake();
     function Lake(){
         var lake = prompt("This is a huge lake.  \n- go swiming");
@@ -96,6 +99,7 @@ function Game(){
             else if( lake = "burn down hut" || lake == "burn"){
                 alert("you burn down the hut... hope you feel good about yourself.. Pyro!");
             }
+            
             else{
                 alert("don't throw rockes at hut ... please!");
             }
@@ -108,8 +112,9 @@ function Game(){
         else{
             alert("I don't understand "+lakeEnv);
             Lake();
-            
+  Castle();
   function Castle(){
+      var gotocastle = prompt("it is huge. /n- look around /n- go inside")
       var insideCastle = prompt(" - upstairs - downstairs - courtyard - balcony - look").tolowerCase();
        
      switch(insideCastle){
@@ -135,7 +140,7 @@ function Game(){
             Castle();
         break;
      }
-  }
+  
         }
     }
 }
