f4cf29b113dce64005e17732970dad9ab255ef2b
emily <emily.b566@stu.nebo.edu>
2019-02-21 10:29:19 -0700

    need to fix grammar

--- game.js
+++ game.js
@@ -26,12 +26,9 @@ var enemy ={
 
 
 
-
 Game();
 
 function Game(){
-    var wizardNames = ["Bathazar", "Mordac", "Grendor","Oroco"];
-    alert("lengend of"+wizardNames[Math.floor(Math.random()* 4)]+"!");
 	
 	document.write("Pandemic Survival!");
 	var playerName = prompt("What is your name?");
@@ -41,12 +38,12 @@ function Game(){
 
 	
 function Prison(){
-		var prison = prompt("There has been a pandemic they had kept the disease in a town that was fenced, you were a guard of the diseased you didn't know how they had plotted a plan to escape. One day when you were giving the sick food and then they grabbed you and drug you somewhere. You just barley woke up. You have no clue where everyone is. You no longer have your safteysuit from the disease on. You look around adn see a purple,green,balck,mark on your arm, it is the rash of the disease. /n-look around /n-go to the grass").toLowerCase();
+		var prison = prompt("There has been a pandemic, they had kept the disease in a town that was fenced. You were a guard of the diseased. You didn't know that they had plotted a plan to escape. One day when you were giving the sick food, they grabbed you and drug you somewhere. You just barley woke up. You have no clue where everyone is. You no longer have your safteysuit from the disease on. You look around adn see a purple,green,balck,mark on your arm, it is the rash of the disease. \n-look around \n-go to the grass").toLowerCase();
     
     
 		
 		if(prison == "look around" || prison == "look"){
-			 var exitprison = prompt(" You sit up from you cot. The door to the room is open. You see vibrant green grass with a lake in the distance.  /n -go to the lake /n -stay in the room a look around the room more"); 
+			 alert(" You sit up from you cot. The door to the room is open. You see vibrant green grass with a lake in the distance."); 
                   
         }
 		
@@ -60,7 +57,7 @@ function Prison(){
             
             }
             else{
-                   alert("game over");
+                alert("Game Over");
             }
             
         }
@@ -81,12 +78,12 @@ function Prison(){
         
         
         if(lakeEnv == "go swiming" || lakeEnv == "swim"){
-            var lake = prompt("You have swam across the lake. In the distance you see a forsest that is close enough to walk to /n- forest");
+            var lake = prompt("You have swam across the lake. In the distance you see a forsest that is close enough to walk to \n- forest \n -cut down trees");
             if(lake == "go to the forest" || lake == "forest"){
-                var forest = prompt("You entered the forest. The  trees are giant. You traveled all day around trees you are getting cold and you need get some rest /n- cut down trees" );
+                alert("You entered the forest. The  trees are giant. You traveled all day around trees you are getting cold and you need get some rest. As  you wake up you feel hungry and see a vending machine." );
             }
             else if( lake == "cut down trees" || lake == "cut"){
-                alert("You have cut down trees with a ace that you had. you found strong ropes on the gorund. You made a good shelter and safe for the night In the morning you walked futher and found a abanded town with food from a vending machine and you are hungry so you get something.!");
+                alert("You have cut down trees with a axe that you had. you found strong ropes on the gorund. You made a good shelter and you are safe for the night. In the morning you walked further and found an abanded town with food from a vending machine and you are hungry so you get something.!");
             }
             
             //else{
@@ -107,7 +104,7 @@ Store()
 function Store(){
             alert("The vending machine is half way full");
             
-                var store = prompt("What would youlike to buy? \n-chips \n-peanuts \n-granola bar \n-leave vending machine");
+                 let store = prompt("What would you like? \n-chips  \n-leave vending machine");
                     if(store == "chips" || store == "chips" && inventory.coins >= 100){
                         var chipsBuy = confirm("Are you sure you want to buy chips");
                             if(chipsBuy){
@@ -119,6 +116,7 @@ function Store(){
                                 inventory.coins = inventory.coins -5;
                                 //displays coins left in account
                                 alert("You have "+inventory.coins+" coins remaining");
+                                
                           
                       }
                                 
@@ -145,32 +143,45 @@ function Store(){
   
   Castle();
   function Castle(){
-      var insideCastle = prompt(" - upstairs -downstairs -courtyard -balcony -look").toLowerCase();
+      var insideCastle = prompt(" You continue along in the forest- north -south -east -west").toLowerCase();
        
     
      switch(insideCastle){
-         case "upstairs" || "go upstairs":
-             var upstairs = prompt("You walk a long flight of stairs to the top floor of the castle.");
+         case "north" || "go north":
+             alert("You continue North and run into a brown bear.");
              
              Castle();
         break;
-        case "downstairs":
-            alert("You go down stairs");
+        case "south":
+            alert("You travel south and run into a moose. then see a building and learn that the doctor is there.");
              
         break;
-        case "courtyard":
-            alert("you go to the courtyard");
+        case "east":
+            alert("You travel east and and find a river");
             Castle();
         break;
-        case "balcony":
-            alert("you go to the balcony");
+        case "west":
+            alert("you go west and come across two run away infected.");
             Castle();
         break;
         default:
-            alert("I don't know waht "+insideCastle+" is!");
+            alert("I don't know what "+insideCastle+" is!");
             Castle();
         break;
      }
+             
+ Doctor();
+ function Doctor(){
+     alert("You have traveled a long distance and you have now met the doctor but because of his captor he may not give you the cure it all depends on his mood.")
+     
+     var mood = [" a good mood you get the cure"," a bad mood he got hurt recently and decides to not give the cure"];
+     alert("he is in"+mood[Math.floor(Math.random()*2)]);
+     
+     alert("congrats you finished the game!");
+     
+     QUIT
+ }
+     
   
 }
 
